449424f4bf68198322ecca4fc34b55bf08fca707
adithragh <adithragh2255@gmail.com>
2023-11-07 00:08:27 +0530

    message

---
+++ src/QuizzApp/QuizzData.js
@@ -0,0 +1,104 @@
+const questions = [
+    {
+        questionText: "Who was the first Prime Minister of India??",
+        answerOptions: [
+            { answerText: "Indira Gandhi", isCorrect: false },
+            { answerText: "Lal Bahadur Shastri", isCorrect: false },
+            { answerText: "Pandit Jawaharlal Nehru", isCorrect: true },
+            { answerText: "Charan Singh", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "Which river is mentioned for the largest number of times in Rig Veda?",
+        answerOptions: [
+            { answerText: "Narmada", isCorrect: false },
+            { answerText: "Indus", isCorrect: true },
+            { answerText: "Kaveri", isCorrect: false },
+            { answerText: "Ganga", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "Buddha was born at ?",
+        answerOptions: [
+            { answerText: "Kushinagara", isCorrect: false },
+            { answerText: "Bodh Gaya", isCorrect: false },
+            { answerText: "Lumbini", isCorrect: true },
+            { answerText: "Saranath", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: ".Who was the ruler of Taxilla when Alexander invaded India?",
+        answerOptions: [
+            { answerText: "Ambhi", isCorrect: true },
+            { answerText: "Porus", isCorrect: false },
+            { answerText: "Seleucus", isCorrect: false },
+            { answerText: "Bimbisara", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "The woman who dominated in the political scenario during the reign of Jehangir was:",
+        answerOptions: [
+            { answerText: "Mumtaz", isCorrect: false },
+            { answerText: "Nur Jehan", isCorrect: true },
+            { answerText: "Jodha Bhai", isCorrect: false },
+            { answerText: "Anarkali", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "The founder of Asiatic Society of Bengal in 1784?",
+        answerOptions: [
+            { answerText: "AO Hume", isCorrect: false },
+            { answerText: "HV Derozio", isCorrect: false },
+            { answerText: "William Jones", isCorrect: true },
+            { answerText: "Warren Hastings", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "Bal Gangadhar Tilak published ‘Mahratha’ in ____ language?",
+        answerOptions: [
+            { answerText: "Hindi", isCorrect: false },
+            { answerText: "English", isCorrect: true },
+            { answerText: "Marathi", isCorrect: false },
+            { answerText: "Urdu", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "Where is the headquarters of State Bank of India?",
+        answerOptions: [
+            { answerText: "Kolkata", isCorrect: false },
+            { answerText: "Chennai", isCorrect: false },
+            { answerText: "Mumbai", isCorrect: true },
+            { answerText: "New Delhi", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "In which year Gandhiji conducted his last Satyagraha?",
+        answerOptions: [
+            { answerText: "1948", isCorrect: true },
+            { answerText: "1945", isCorrect: false },
+            { answerText: "1946", isCorrect: false },
+            { answerText: "1947", isCorrect: false },
+        ],
+    },
+
+    {
+        questionText: "Who raised the slogan ‘Inquilab Zindabad’ for the first time?",
+        answerOptions: [
+            { answerText: "Lajpath Rai ", isCorrect: false },
+            { answerText: " Bhagat Singh", isCorrect: true },
+            { answerText: "Motilal Nehru", isCorrect: false },
+            { answerText: "Subhas Chandra Bose", isCorrect: false },
+        ],
+    },
+];
+
+export default questions;
+
